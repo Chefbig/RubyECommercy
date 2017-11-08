f126ff5964c292ee3cd63248c2ee91b88b2aa0e1
Nico Cai <chefbig@gmail.com>
2017-11-08 05:05:33 +0000

    Add models Product and Image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108042823) do
+ActiveRecord::Schema.define(version: 20171108050354) do
 
   create_table "abouts", force: :cascade do |t|
     t.string "name"
@@ -62,6 +62,23 @@ ActiveRecord::Schema.define(version: 20171108042823) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "file_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "stock_quantity"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
