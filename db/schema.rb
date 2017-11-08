d86b9994169a192813e174aeaa5cb2bd881b98de
Nico Cai <chefbig@gmail.com>
2017-11-08 05:25:13 +0000

    Add models Product and Image, add references to product in Images, add active_admin to models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108050354) do
+ActiveRecord::Schema.define(version: 20171108050857) do
 
   create_table "abouts", force: :cascade do |t|
     t.string "name"
@@ -70,6 +70,8 @@ ActiveRecord::Schema.define(version: 20171108050354) do
     t.string "file_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "product_id"
+    t.index ["product_id"], name: "index_images_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
