4cecbcde0202012f6592c06724953bbbba6943e2
Nico Cai <chefbig@gmail.com>
2017-11-15 20:20:34 +0000

    add categories into search box

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,15 @@
 class HomeController < ApplicationController
   def index
     #@products = Product.all
+    @categories = Category.all.order(:id)
     @products = Product.order(:id).page params[:page]
+
   end
 
   def about
     @about = About.first
     @address = Address.first
   end
+
+
 end
