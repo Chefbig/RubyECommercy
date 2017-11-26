9c93de8107074d9a5dc04207a1964c1bc457bc3e
Nico Cai <chefbig@gmail.com>
2017-11-26 08:44:28 +0000

    Added address to user, bind address to shopping cart

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,7 +3,7 @@ class AddressesController < InheritedResources::Base
   private
 
     def address_params
-      params.require(:address).permit(:firstname, :lastname, :phone, :addressline1, :addressline2, :city, :postalcode, :is_billing_address, :is_shipping_address, :user_id)
+      params.require(:address).permit(:firstname, :lastname, :phone, :addressline1, :addressline2, :city, :postalcode, :is_billing_address, :is_shipping_address, :user_id, :province_id)
     end
 end
 
