4816ccdb5d4da0e4c62ee504805b0099aae3b9e8
Nico Cai <chefbig@gmail.com>
2017-11-27 13:00:47 +0000

    categories search fixed, all category, order by name

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,11 @@
 class CategoriesController < ApplicationController
   def show
-    @categories = Category.find(params[:id])
-    @products = @categories.product
+    if (params[:id]) != '10'
+      @categories = Category.find(params[:id])
+      @products = @categories.product.distinct
+    else
+      @products = Product.all.order(:name)
+    end
+
   end
 end
