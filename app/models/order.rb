4f6c8ac01ecb92aa2b0d3c39d9e3f570a1e5e55f
Nico Cai <chefbig@gmail.com>
2017-11-28 07:58:51 +0000

    fixed user-roders association

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
   has_many :line_items
+  belongs_to :user
+  belongs_to :province
 
   validates :total, :state, :gst, :pst, :hst, :qst, :payment, presence: true
   validates :total, :gst, :pst, :hst, :qst, :payment,  numericality: { greater_than_or_equal_to: 0 }
